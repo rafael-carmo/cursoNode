aa0abfce3d2e23727817023ae9a52efd2f1ad993
rafael <rafaelljj@gmail.com>
2022-08-03 11:23:53 -0300

    Correções

--- 15_GET_A_PET/backend/helpers/get-user-by-token.js
+++ 15_GET_A_PET/backend/helpers/get-user-by-token.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken")
 const User = require('../models/User')
 
 const getUserByToken = async (token) => {
+    
     if(!token){
         return res.status(401).json({message: 'Acesso negado!'})
     }
